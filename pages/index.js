00ff01b03082cab0184d3614c9aded9e6740fea0
agomez99 <austine.gomez@yahoo.com>
2023-06-19 01:56:52 -0500

    5 files modified, 4 added, 4 deleted

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,25 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+      <title>Frontend Mentor | Tip calculator app</title>
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         SPITTER
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
