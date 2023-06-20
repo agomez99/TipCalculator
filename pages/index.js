34d34ea8c9b9b067e61d5680eaaf1b8e1e93e10d
Austine Gomez <austine.gomez@yahoo.com>
2023-06-19 22:49:00 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,45 @@
-import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Col, Row, Container, Card, Form, Button} from 'react-bootstrap';
-import { useState } from 'react';
+import styles from '../styles/Home.module.css';
+
 export default function Home() {
-  const [billAmount, setBillAmount] = useState('')
-  const [tipPercentage, setTipPercentage] = useState('')
-  const [numPeople, setNumPeople] = useState('')
+
+  const [billAmount, setBillAmount] = useState(0);
+  const [tipPercentage, setTipPercentage] = useState(0);
+  const [numPeople, setNumPeople] = useState(1);
 
   const handleCustomTip = () => {
-    const customTip = prompt('Enter custom tip percentage')
-    if (customTip !== null) { setTipPercentage(parseFloat(customTip)) }
-    else {
-      handleReset()
+    const customTip = prompt('Enter custom tip percentage');
+    if (customTip !== null) {
+      setTipPercentage(parseFloat(customTip));
+    } else {
+      reset();
     }
-  }
-  const reset = () => {
-    setBillAmount('')
-    setTipPercentage('')
-    setNumPeople('')
-  }
+  };
 
+  const reset = () => {
+    setBillAmount(0);
+    setTipPercentage(0);
+    setNumPeople(1);
+  };
+
+  const totalTipAmount = () => {
+    let total = (billAmount * tipPercentage) / 100;
+    return total.toFixed(2);
+  };
+
+  const totalAmountPerPerson = () => {
+    let total = (billAmount / numPeople) + ((billAmount * tipPercentage) / (100 * numPeople));
+    return total.toFixed(2);
+  };
 
   return (
     <>
       <Head>
         <title>Frontend Mentor | Tip calculator app</title>
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/images/favicon-32x32.png'
-        />
+        <link rel='icon' type='image/png' sizes='32x32' href='/images/favicon-32x32.png' />
       </Head>
       <Container fluid className={styles.container}>
         <main className={styles.main}>
@@ -42,7 +50,7 @@ export default function Home() {
           <Card className={styles.card}>
             <Row>
               <Col md={6} sm={12} className={styles.tipAmount}>
-                <Form >
+                <Form>
                   <Form.Group className='mb-3' controlId='formBill'>
                     <Form.Label>Bill</Form.Label>
                     <Form.Control
@@ -50,14 +58,13 @@ export default function Home() {
                       placeholder='$'
                       id='billAmount'
                       value={billAmount}
-                      style={{ width: '100%', height:'50px' }}
+                      style={{ width: '100%', height: '50px' }}
                       onChange={(e) => setBillAmount(e.target.value)}
                     />
                   </Form.Group>
                   <p>Select Tip %</p>
 
                   <div className={styles.tipButtons}>
-
                     {[5, 10, 15, 25, 50].map((tip) => (
                       <Button
                         key={tip}
@@ -67,16 +74,12 @@ export default function Home() {
                         {`${tip}%`}
                       </Button>
                     ))}
-
-                    <Button
-                      className={styles.button}
-                      onClick={handleCustomTip}
-                    >
+                    <Button className={styles.button} onClick={handleCustomTip}>
                       Custom
                     </Button>
                   </div>
-                        
-                  <Form.Group  controlId='formNumPeople'>
+
+                  <Form.Group controlId='formNumPeople'>
                     <Form.Label>Number of People</Form.Label>
                     <Form.Control
                       type='number'
@@ -85,7 +88,7 @@ export default function Home() {
                       value={numPeople}
                       onChange={(e) => setNumPeople(e.target.value)}
                       className={styles.input}
-                      style={{ width: '100%', height:'50px' }}
+                      style={{ width: '100%', height: '50px' }}
                       min={1}
                       max={100}
                       step={1}
@@ -96,25 +99,25 @@ export default function Home() {
                 </Form>
               </Col>
               <Col md={6} sm={12}>
-                  <div className={styles.totalAmount}>
-                      <p>Total ${billAmount + ((billAmount * tipPercentage) / 100)}</p>
-                      <div className={styles.tipText}>
-                          <p>Tip Amount</p>
-                          <p>/ per person</p>
-                          <p className={styles.tipPersonText}>${((billAmount * tipPercentage) / (100 * numPeople))|| 0}</p>
-                      </div>
-                      <p className={styles.tipText}>
-                          Total / per Person <span className={styles.totalPersonText}> ${((billAmount * tipPercentage) / (100 * numPeople) + (billAmount / numPeople)) || 0  } </span>
-                      </p>
-                      <Button
-                          className={styles.button}
-                          onClick={reset}
-                          style={{ width: '90%' }}
-                      >
-                          RESET
-                      </Button>
+                <div className={styles.totalAmount}>
+                  <p>Total ${billAmount + ((billAmount * tipPercentage) / 100)}</p>
+                  <div className={styles.tipText}>
+                    <p>Tip Amount</p>
+                    <p>/ per person</p>
+                    <p className={styles.tipPersonText}>{(totalTipAmount() / numPeople).toFixed(2)}</p>
                   </div>
-</Col>
+                  <p className={styles.tipText}>
+                    Total / per Person <span className={styles.totalPersonText}>{(totalAmountPerPerson())}</span>
+                  </p>
+                  <Button
+                    className={styles.button}
+                    onClick={reset}
+                    style={{ width: '90%' }}
+                  >
+                    RESET
+                  </Button>
+                </div>
+              </Col>
 
             </Row>
           </Card>
